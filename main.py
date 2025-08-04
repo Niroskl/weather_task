e77a905cd90f6153a7ccdeadf51e9d27c9da710e
Niroskl <nirskalka@gmail.com>
2025-08-04 10:04:09 +0300

    improve code with guidelines

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+# The scope of this project: a code return current temperature, weather conditions, and humidity from any city in the world from OpenWeather site
+# I wrote the code using Colab and then used PyCharm to synchronize with GitHub platform
 
+# Steps:
+#   I open an account on OpenWeather site
+#   personal API key obtained from site: your API key 81661abd3ae3f6f042b8111f19aa7a9f
+#   I checked in the site for examples and templates for how does API call looks like:
+#       Example of API call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=81661abd3ae3f6f042b8111f19aa7a9f
+#       Template fits for this code needs: http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+# code:
+
+# first I imported the requests library to be able to extract API from open world weather site
+# at the terminal tab I wrote: pip install requests followed by -
 import requests
 
 city_name = input('please enter city name you would like to know about the local weather: ')
